ddaf5200e13c4619cd2a2e5bfaae16cd32c839d5
Martin Echavez <39717277+martinEchavez@users.noreply.github.com>
2020-04-06 08:00:25 -0500

    Update 04_funciones.py

--- 04_funciones.py
+++ 04_funciones.py
@@ -1,21 +1,22 @@
+#Las funciones se definen con la palabra reservada def
 def mostar_nombre():
  print('Martin Elias')
-
+#Llamamor a la funcion
 mostar_nombre()
 
-#Funciones con parametros
+#Funciones con parámetros
 def informacion(nombre, genero):
  print(f'Mi nombre es {nombre} y soy de genero {genero}')
-
+#Le pasamos los parámetros a la función
 informacion('Juan Ramires', 'Masculino')
 informacion('Alicia Campos', 'Femenina')
 
-#Funciones con parametros por defecto
+#Funciones con parámetros por defecto
 def informacion_personas(nombre, genero = 'Otro'):
  print(f'Mi nombre es {nombre} y soy de genero {genero}')
 
 informacion_personas('Juan Ramires', 'Masculino')
-informacion_personas('Alicia Campos')
+informacion_personas('Alicia Campos') #Si no le asignamos un género el toma por defecto Otro
 
 #Funciones que retornan un valor
 def persona(nombre):
@@ -23,7 +24,7 @@ def persona(nombre):
 
 persona1 = persona('Martin')
 print(persona1)
-
+#Funcion para sumar dos valores
 def suma(a,b):
  return a + b
 
